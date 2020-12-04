5f9ded3cf5034115f2f746d4395361162f5ec4c9
Tinh-Chi TRAN <tinhchi.tran@gmail.com>
2020-12-04 23:06:54 +0700

    Added versioneer for `core-service-python` service, with an initial `README.md` and the `requirements.txt` file

---
+++ services/core-service-python/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+import versioneer
+
+
+with open('README.md', 'r') as fh:
+	long_description = fh.read()
+
+with open('requirements.txt', 'r') as f:
+	requirements = [line for line in f.readlines() if len(line) > 0 and not 'oandapy' in line]
+
+
+setuptools.setup(
+	name='prime_finder_core',
+	version=versioneer.get_version(),
+	cmdclass=versioneer.get_cmdclass(),
+	author='Tinh-Chi TRAN',
+	author_email='tinhchi.tran@gmail.com',
+	description='Core-serivce for largest prime finder',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	url='https://github.com/dinosaurchi/stably-interview',
+	packages=setuptools.find_packages(exclude=['docs', 'testcases']),
+	install_requires=requirements,
+	classifiers=[
+		'Programming Language :: Python :: 3',
+		'Operating System :: OS Independent',
+	],
+	python_requires='>=3.6,<3.8',
+	include_package_data=True,
+	zip_safe=True
+)
