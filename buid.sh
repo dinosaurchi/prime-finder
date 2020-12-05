0c3fe6cd17f45a6d573d98851e4a912aa178ce32
Tinh-Chi TRAN <tinhchi.tran@gmail.com>
2020-12-05 17:58:15 +0700

    Added --build-arg for docker-compose build to insert build argument correctly

--- buid.sh
+++ buid.sh
@@ -1,3 +1,6 @@
 source .env
 
-COMPOSE_DOCKER_CLI_BUILD=1 docker-compose build
+COMPOSE_DOCKER_CLI_BUILD=1 docker-compose build \
+	--build-arg CLIENT_SERVICE_DIR="$CLIENT_SERVICE_DIR" \
+	--build-arg CORE_SERVICE_DIR="$CORE_SERVICE_DIR" \
+	--build-arg PROTO_DIR="$PROTO_DIR"
