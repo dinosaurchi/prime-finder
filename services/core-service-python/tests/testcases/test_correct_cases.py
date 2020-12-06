cc0110c6f33d509632c66db5a9a8d7538ed11d9d
Tinh-Chi TRAN <tinhchi.tran@gmail.com>
2020-12-07 00:51:14 +0700

    Added calculating total algorithm runtime for correctness checking unittest, as we compare the algorithm with a very simple (but very slow) primarity checking algorithm. Also, we have a backward test to find out if there exists a prime number higher than the output result, but still lower than input number or not. Thus, it is better the separate the runtime of the target algorithm and the test-case check algorithm

--- services/core-service-python/tests/testcases/test_correct_cases.py
+++ services/core-service-python/tests/testcases/test_correct_cases.py
@@ -5,6 +5,7 @@ from prime_finder_core.global_vars import TEST_CORE_SERVICE_ADDRESS
 from tqdm import tqdm
 import unittest
 import grpc
+import timeit
 
 
 def is_prime(n):
@@ -28,13 +29,17 @@ class Test(unittest.TestCase):
 		values += [
 			5223444,
 		]
+		total_time = 0
 		for i in tqdm(values):
 			cur_request = get_largest_prime_pb2.InputRequest(
 				value=i
 			)
+			start = timeit.default_timer()
 			output = stub.GetLargestPrime(cur_request)
+			total_time += timeit.default_timer() - start
 			self.assertTrue(output.status, 'Must have true status: {v}'.format(v=i))
 			self.assertTrue(is_prime(n=output.value), msg='Checking highest lower prime: {j} (n={n})'.format(j=output.value, n=i))
 
 			for j in range(output.value + 1, i):
 				self.assertFalse(is_prime(n=j), msg='Checking higher prime: {j} (n={n})'.format(j=j, n=i))
+		print('Total algorithm runtime: {total_time}'.format(total_time=total_time))
