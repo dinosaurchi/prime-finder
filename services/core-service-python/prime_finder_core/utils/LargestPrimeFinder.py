3e58922b5cd807e8e888e2fdf81d6f1b465e6adf
Tinh-Chi TRAN <tinhchi.tran@gmail.com>
2020-12-07 00:42:17 +0700

    Implemented Sieve_of_Sundaram algorithm to speed up the prime checking algorithm for the first 10 millions integer
    Closes #16

--- services/core-service-python/prime_finder_core/utils/LargestPrimeFinder.py
+++ services/core-service-python/prime_finder_core/utils/LargestPrimeFinder.py
@@ -3,7 +3,29 @@ import numpy as np
 
 class LargestPrimeFinder:
 	def __init__(self):
-		pass
+		self.__marked = None
+		# Init for the first 10 millions integers as the algorithm
+		self.__update_prime_db(n=1e7)
+
+	def __update_prime_db(self, n:int):
+		'''
+		Sieve_of_Sundaram algorithm
+		- Reference: https://en.wikipedia.org/wiki/Sieve_of_Sundaram
+		'''
+		if n < 3:
+			return
+
+		n = int((n - 1) / 2)
+		self.__marked = [False] * (n + 1)
+
+		for i in range(1, n + 1):
+			j = i
+			while True:
+				temp = i + j + 2 * i * j
+				if temp  > n:
+					break
+				self.__marked[temp] = True
+				j += 1
 
 	def __is_prime(self, n:int):
 		if n <= 1:
@@ -13,12 +35,9 @@ class LargestPrimeFinder:
 		if n > 2 and n % 2 == 0:
 			return False
 
-		max_n = int(np.floor(np.sqrt(n)))
-		# Check all the odd numbers
-		for i in range(3, 1 + max_n, 2):
-			if n % i == 0:
-				return False
-		return True
+		check_index = int((n - 1) / 2)
+		# Part of Sieve_of_Sundaram algorithm
+		return not self.__marked[check_index]
 
 	def get_lower_largest_prime(self, n:int):
 		original_n = n
